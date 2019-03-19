68e06e1f52e54f82ccb011a694466481bdbf75dc
Ollila <sami-pekka.ollila@cgi.com>
2019-03-19 16:00:04 +0200

    Created a database and model for get/post studios. Task #7

---
+++ questspot_backend/models/studio.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const studioSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  passwordHash: String,
+  name: String,
+  address: String,
+  postalCode: String,
+  city: String,
+  country: String,
+  artists: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Artist'
+    }
+  ],
+  images: [],
+  tags: []
+})
+
+studioSchema.plugin(uniqueValidator)
+studioSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+const Studio = mongoose.model('Studio', studioSchema)
+
+module.exports = Studio
