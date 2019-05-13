38b4b63d6e0ee44ba7cbcb13c8f909b022c2665f
teodosin <vik.teodosin@gmail.com>
2019-05-13 17:55:03 +0300

    *Header fully functional

--- questspot_frontend/src/components/Sidebar.js
+++ questspot_frontend/src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React , { useState }  from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { slide as Menu } from 'react-burger-menu'
 import { Link, withRouter } from 'react-router-dom'
@@ -20,7 +20,7 @@ const Sidebar = (props) => {
   const handleStateChange = (state) => {
     setIsMenuOpen(state.isOpen)
   }
-  
+
   // This can be used to close the menu, e.g. when a user clicks a menu item
   const closeMenu = () => {
     setIsMenuOpen(false)
@@ -42,44 +42,86 @@ const Sidebar = (props) => {
         </Link>
       )
     }
-    return (          
+    return (
       <p>
         <Button variant="link" onClick={() => showLogIn()}>Log in</Button>
       </p>
     )
   }
   return (
-    
+
     <div className="menu-background">
-      <Menu isOpen={ isMenuOpen } onStateChange={(state) => handleStateChange(state)}>
+      <Menu isOpen={isMenuOpen} onStateChange={(state) => handleStateChange(state)}>
         {renderUsername()}
         <Authentication />
         <Link className="menu-item" onClick={() => closeMenu()} to="/">
-            Home
+          Home
         </Link>
         <Link className="menu-item" onClick={() => closeMenu()} to="/profiles/">
-            Search
+          Search
         </Link>
       </Menu>
-      <Headroom style={{height: '5em'}}>
-          <HeaderMenu />
+      <Headroom >
+        <HeaderMenu props={props} alt={true} />
       </Headroom>
     </div>
 
   )
 }
 
-const HeaderMenu = () => {
+const HeaderMenu = ({ props, alt }) => {
+  if (alt) return (
+    <div className='headerBoxAlt' height='5em'>
+      <svg id='headerBack' width='100%' height='5em'>
+        <rect height='100%' width='100%' fill='#436151' />
+        <rect height='4.8em' x='15%' width='70%' fill='#f0f0db' />
+        <rect height='4.8em' width='100%' fill='#436151' y='-.2em' />
+      </svg>
 
-  return (
-    <div width='100%' height='5em'>
-      <svg width='100%' height='5em'>
-        <rect width='100%' height='50%' fill='green' />
-        <ellipse cx='50%' cy='50%' rx='40%' ry='50%' fill='green' />
+      <img src={logo} className='app-logo' alt="GuestSpot" />
+      <div className="shortcuts">
+        <Link className={`shortcut-item ${props.location.pathname === '/' ? 'selected' : 'not-selected'}`} to="/">
+          <i className="far fa-newspaper"></i>
+        </Link>
+
+        <Link className={`shortcut-item ${props.location.pathname === '/profiles/' ? 'selected' : 'not-selected'}`} to="/profiles/">
+          <i className="fas fa-search"></i>
+        </Link>
+      </div>
+
+      <svg id='headerFront' width='100%' height='5em'>
+        <mask id='maskBack' fill='#fff'>
+          <rect id='boxBack' width='100%' height='4.8em' />
+          <ellipse id='ellBack' fill='#000' cx='50%' cy='100%' rx='6.3em' ry='4.5em' />
+        </mask>
+        <use href='#boxBack' mask='url(#maskBack)' fill='#f0f0db' />
+
+        <mask id='maskFront' fill='#fff'>
+          <rect id='boxFront' width='100%' height='4.8em' />
+          <ellipse id='ellFront' fill='#000' cx='50%' cy='100%' rx='6.5em' ry='4.7em' />
+        </mask>
+        <use href='#boxFront' mask='url(#maskFront)' fill='#436151' />
       </svg>
     </div>
   )
+
+
+  return (
+    <div className='headerBox' >
+      <svg width='100%' height='5em'>
+        <rect height='100%' width='100%' fill='#f0f0db' />
+        <rect height='100%' width='100%' fill='#436151' y='-.2em' />
+        <rect id='boxBack' width='100%' height='50%' />
+        <ellipse id='ellBack' cx='50%' cy='30%' rx='7em' ry='70%' />
+        <rect id='boxFront' width='100%' height='50%' />
+        <ellipse id='ellFront' cx='50%' cy='30%' rx='6.8em' ry='70%' />
+      </svg>
+      <img src={logo} className='app-logo' alt="GuestSpot" />
+    </div>
+  )
 }
+
+
 
 /* Old header
 <div className="top-container">
